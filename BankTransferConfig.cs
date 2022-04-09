f60869df015a73172533f7efb45015e7926180a3
Aditya MP <211wadityaw112@gmail.com>
2022-04-09 14:55:42 +0700

    “aplikasi bank transfer dengan runtime configuration”.

---
+++ BankTransferConfig.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+
+namespace com.modul7_1302204054
+{
+
+	public class BankTransferConfig
+	{
+		//Set Default Value
+		public string CONFIG1 { get; set; } = "en";
+		public int CONFIG2 { get; set; } = 25000000;
+		public int CONFIG3 { get; set; } = 6500;
+		public int CONFIG4 { get; set; } = 15000;
+		public string[] CONFIG5 { get; set; } = new string[] { "RTO(real - time)", "SKN", "RTGS", "BI FAST" };
+		public string CONFIG6 { get; set; } = "yes";
+		public string CONFIG7 { get; set; } = "ya";
+
+		//Class Attributes
+		public string lang { get; set; }
+		public Transfer transfer { get; set; }
+		public string[] methods { get; set; }
+		public Confirmation confirmation { get; set; }
+
+
+
+
+
+		public void Load()
+		{
+			try
+			{
+				string jsonText = File.ReadAllText("bank_transfer_config.json");
+				var json = JsonConvert.DeserializeObject<BankTransferConfig>(jsonText);
+			
+				this.lang = json.lang;
+				this.transfer = json.transfer;
+				this.methods = json.methods;
+				this.confirmation = json.confirmation;
+
+            } catch (Exception ex)
+            {
+				Console.WriteLine(ex.Message);
+            }
+		}
+		public class Transfer
+		{
+			public int CONFIG2 { get; set; } = 25000000;
+			public int CONFIG3 { get; set; } = 6500;
+			public int CONFIG4 { get; set; } = 15000;
+			public int threshold { get; set; }
+			public int low_fee { get; set; }
+			public int high_fee { get; set; }
+		}
+
+		public class Confirmation
+		{
+			public string CONFIG6 { get; set; } = "yes";
+			public string CONFIG7 { get; set; } = "ya";
+			public string en { get; set; }
+			public string id { get; set; }
+		}
+	}
+
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,70 @@ namespace com.modul7_1302204054
     {
         public static void Main()
         {
-            //TODO program logic here
+            BankTransferConfig config = new BankTransferConfig();
+            config.Load(); 
+
+            if (config.lang == "id") {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer");
+            } else
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            int input1 = int.Parse(Console.ReadLine());
+
+            int biayaTransfer;
+            if(input1 <= config.transfer.threshold)
+            {
+                biayaTransfer = config.transfer.low_fee;
+            } else
+            {
+                biayaTransfer = config.transfer.high_fee;
+            }
+
+            if (config.lang == "id")
+            {
+                Console.WriteLine($"Total biaya = {input1 + biayaTransfer}");
+                Console.WriteLine("Pilih metode transfer:");
+            }
+            else
+            {
+                Console.WriteLine($"Total amount = {input1 + biayaTransfer}");
+                Console.WriteLine("Select transfer method:");
+            }
+            
+            int iter = 1;
+            foreach(string item in config.methods)
+            {
+                Console.WriteLine(iter.ToString() + ". " + item);
+                iter++;
+            }
+            string input2 = Console.ReadLine();
+
+            if (config.lang == "id")
+            {
+                Console.WriteLine($"Ketik { config.confirmation.id } untuk mengkonfirmasi transaksi:");
+                if(Console.ReadLine() == config.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                } else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Please type { config.confirmation.en } to confirm the transaction:");
+                if (Console.ReadLine() == config.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+
+
         }
     }
 }
